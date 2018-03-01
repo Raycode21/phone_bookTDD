0b31a3ce9e496f4971f40c44b42576211c535063
raycode21 <wanjiku.rachel21@gmail.com>
2018-03-01 19:08:40 +0300

    add new duplicate feature

--- phone_app/test_contacts.py
+++ phone_app/test_contacts.py
@@ -12,40 +12,38 @@ class PhoneBookTest(unittest.TestCase):
 		
 
 	def test_add_contact(self):
-
-		new_contact = contact.add_contact("Mary", 7245698500)
+		new_contact = contact.add_contact("Mary", 07245698500)
 		current_contactList = contactList.append(new_contact)
 		self.assertEqual(len(current_contactList) - self.contacts_count, 1)
 
 
 
-		
 	def test_update_contact(self):
-		contact = Contact("Mary",256398742)
-		updated_contact = contact.update_contact("Mary", 789654123)
+		contact = Contact("Mary",076398742)
+		updated_contact = contact.update_contact("Mary", 0789654123)
 		self.assertNotEqual(contact, updated_contact, msg= "no change made")
-
-
-	def test_input_type(self):
-		contact3 = contact.create_contact(0123, 777896589)
-
-
 
 	
 		
 	def test_delete_contact(self):
-		contact3 = Contact("Ary", 890754545)
+		contact3 = Contact("Ary", 07230754545)
 		contact3.delete_contact()
 		self.assertEqual(len(contactList), 49)
 		
 
 	def test_view_contact(self):
-		contact4 = Contact("Lillian", 78903455)
+		contact4 = Contact("Lillian", 078903455)
 		contact4.test_view_contact(Lillian)
-		self.assertEqual({"Lillian": 78903455}, msg="not found")
+		self.assertEqual({"Lillian": 78903455}, msg="contact not found")
 		
 
 	def test_duplicate_contacts(self):
+		contact5 = contact5("Olive",072569983)
+		contact6 = contact5("Olive", 072569983)
+		error = "contact already exists"
+		self.assertEqual(error, contact6)
+
+		
 		
 
 
